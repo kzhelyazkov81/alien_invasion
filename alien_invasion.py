5d3e456cfbc9daeb0ad47b05e79c6ee5255ae3b7
kzhelyazkov81 <kzhelyazkov81@gmail.com>
2022-04-21 14:11:31 +0300

    update

--- alien_invasion.py
+++ alien_invasion.py
@@ -58,6 +58,7 @@ class AlienInvasion:
         """Start a new game when the player clicks Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
             self.aliens.empty()
@@ -112,6 +113,8 @@ class AlienInvasion:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
+
 
 
     def _update_aliens(self):
